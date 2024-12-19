245e094ec8deb1966fce7defb61048520b870685
Grigory Mikheev <grigory.mikheev@gmail.com>
2024-12-19 23:48:17 +0000

    Commit to github, clean up platform-specific code

--- query_executor.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-#include <set>
-#include "boost/date_time/local_time/local_time.hpp"
-
-std::set<std::pair<std::string, std::string>> getSlotsFiltered(const std::set<std::string>& venues);
-auto parseSlots(std::string_view json) -> std::set<boost::posix_time::ptime>;
-
-
